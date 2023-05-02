ba608d55fa60641f64bc2335f8dda7d626544a7f
Luis Florez <braian1k3@gmail.com>
2023-05-02 17:49:52 -0500

    Add new class "UserController".

---
+++ src/main/java/com/users/ProyectUsers/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.users.ProyectUsers.controller;
+
+import com.users.ProyectUsers.entity.User;
+import com.users.ProyectUsers.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/users/new")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        User request = userService.saveUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(request);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUser (@PathVariable String id_user) {
+        User user = userService.getUser(id_user);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("users/findAll")
+    public ResponseEntity<List<User>> getAllUsers () {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+}
