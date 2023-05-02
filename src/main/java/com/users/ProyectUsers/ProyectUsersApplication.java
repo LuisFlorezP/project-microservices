73bb73959f011eb43a0edaf160854755b019e227
Luis Florez <braian1k3@gmail.com>
2023-05-02 16:38:28 -0500

    New proyect with Spring Initializr.

---
+++ src/main/java/com/users/ProyectUsers/ProyectUsersApplication.java
@@ -0,0 +1,13 @@
+package com.users.ProyectUsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/users/ProyectUsers/ProyectUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.users.ProyectUsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
