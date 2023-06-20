dc741932262fea8a16655dbd529b23d2f0cce969
Luis Florez <braian1k3@gmail.com>
2023-06-20 12:32:06 -0500

    Generate new service to hotel.

---
+++ ServiceHotel/src/main/java/com/hotel/ProjectHotels/ProjectHotelsApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.ProjectHotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHotelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHotelsApplication.class, args);
+	}
+
+}
---
+++ ServiceHotel/src/test/java/com/hotel/ProjectHotels/ProjectHotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.ProjectHotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHotelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
