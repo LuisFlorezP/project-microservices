d4230a9f2b955b9995ef23761705e2812fee6c41
Luis Florez <braian1k3@gmail.com>
2023-06-20 12:10:30 -0500

    Document the controller a bit more UserController.

--- src/main/java/com/users/ProyectUsers/controller/UserController.java
+++ src/main/java/com/users/ProyectUsers/controller/UserController.java
@@ -2,6 +2,9 @@ package com.users.ProyectUsers.controller;
 
 import com.users.ProyectUsers.entity.User;
 import com.users.ProyectUsers.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,24 +13,28 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "Services of Users", description = "Services to the User entity.")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping("/users/new")
+    @Operation(summary = "Method to save a user register.")
+    @PostMapping("/new")
     public ResponseEntity<User> saveUser(@RequestBody User user) {
         User request = userService.saveUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(request);
     }
 
-    @GetMapping("/users/{idUser}")
+    @Operation(summary = "Method to get a user register.")
+    @GetMapping("/{idUser}")
     public ResponseEntity<User> getUser (@PathVariable String idUser) {
         User user = userService.getUser(idUser);
         return ResponseEntity.ok(user);
     }
 
-    @GetMapping("users/findAll")
+    @Operation(summary = "Method to get all user registers.")
+    @GetMapping("/findAll")
     public ResponseEntity<List<User>> getAllUsers () {
         List<User> users = userService.getAllUsers();
         return ResponseEntity.ok(users);
