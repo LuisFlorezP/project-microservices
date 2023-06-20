d5b5619b2264917c892fe118666248c8d65f095a
Luis Florez <braian1k3@gmail.com>
2023-06-20 13:13:12 -0500

    Add new class "Hotel".

---
+++ ServiceHotel/src/main/java/com/hotel/ProjectHotels/entity/Hotel.java
@@ -0,0 +1,45 @@
+package com.hotel.ProjectHotels.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "hotel_entity")
+public class Hotel {
+
+    @Id
+    @Column(name = "id_hotel")
+    private String idHotel;
+    private String information;
+    private String location;
+
+    public String getIdHotel() {
+        return idHotel;
+    }
+
+    public void setIdHotel(String idHotel) {
+        this.idHotel = idHotel;
+    }
+
+    public String getInformation() {
+        return information;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
