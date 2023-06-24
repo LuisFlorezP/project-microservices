02d028e57a294fb724991cc23ac5edd33211d047
Luis Florez <braian1k3@gmail.com>
2023-06-24 15:40:45 -0500

    Add annotation of Eureka in "ProjectRegistersApplication" and properties of port and servlet for the Register service.

--- RegisterEureka/src/main/java/com/register/ProjectRegisters/ProjectRegistersApplication.java
+++ RegisterEureka/src/main/java/com/register/ProjectRegisters/ProjectRegistersApplication.java
@@ -2,8 +2,10 @@ package com.register.ProjectRegisters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ProjectRegistersApplication {
 
 	public static void main(String[] args) {
