a99f939048725221cf608e478126af618775bdcd
Luis Florez <braian1k3@gmail.com>
2023-06-24 15:26:24 -0500

    Generate new service to connect the services in the Eureka server.

---
+++ RegisterEureka/src/main/java/com/register/ProjectRegisters/ProjectRegistersApplication.java
@@ -0,0 +1,13 @@
+package com.register.ProjectRegisters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRegistersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRegistersApplication.class, args);
+	}
+
+}
---
+++ RegisterEureka/src/test/java/com/register/ProjectRegisters/ProjectRegistersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.register.ProjectRegisters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRegistersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
