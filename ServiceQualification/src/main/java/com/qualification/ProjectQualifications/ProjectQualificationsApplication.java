79f736705b3168dcfda663fe50aa10539e701c3b
Luis Florez <braian1k3@gmail.com>
2023-06-24 13:08:39 -0500

    Generate new service to "Qualification".

---
+++ ServiceQualification/src/main/java/com/qualification/ProjectQualifications/ProjectQualificationsApplication.java
@@ -0,0 +1,13 @@
+package com.qualification.ProjectQualifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectQualificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectQualificationsApplication.class, args);
+	}
+
+}
---
+++ ServiceQualification/src/test/java/com/qualification/ProjectQualifications/ProjectQualificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qualification.ProjectQualifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectQualificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
