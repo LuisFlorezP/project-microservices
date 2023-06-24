07c32ce26d734991ddd1bc9b9eb4e5b02fe4aac7
Luis Florez <braian1k3@gmail.com>
2023-06-24 18:14:01 -0500

    Add the process for assign a hotel to a qualification and also assign qualifications to a user.

--- ServiceUser/src/main/java/com/users/ProyectUsers/service/impl/UserServiceImpl.java
+++ ServiceUser/src/main/java/com/users/ProyectUsers/service/impl/UserServiceImpl.java
@@ -1,19 +1,27 @@
 package com.users.ProyectUsers.service.impl;
 
-import com.users.ProyectUsers.entity.User;
+import com.users.ProyectUsers.entities.Hotel;
+import com.users.ProyectUsers.entities.Qualification;
+import com.users.ProyectUsers.entities.User;
 import com.users.ProyectUsers.exceptions.ResourceNotFoundException;
 import com.users.ProyectUsers.repository.UserRepository;
 import com.users.ProyectUsers.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Override
     public User saveUser(User user) {
@@ -29,7 +37,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String idUser) {
-        return userRepository.findById(idUser)
+        User user = userRepository.findById(idUser)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found with the id: " + idUser));
+        Qualification[] qualificationsUser = restTemplate.getForObject("http://localhost:8083/qualificationsUdemy/qualifications/users/" + idUser, Qualification[].class);
+        List<Qualification> qualifications = Arrays.stream(qualificationsUser).collect(Collectors.toList());
+        for (Qualification qualification : qualifications) {
+            Hotel hotelQualification = restTemplate.getForObject("http://localhost:8082/hotelsUdemy/hotels/" + qualification.getHotelId(), Hotel.class);
+            qualification.setHotel(hotelQualification);
+        }
+        user.setQualifications(qualifications);
+        return user;
     }
 }
