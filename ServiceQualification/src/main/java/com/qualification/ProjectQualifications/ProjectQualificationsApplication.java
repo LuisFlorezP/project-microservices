8dab30ddb2bedeec882e4909c747ee9bab3cf493
Luis Florez <braian1k3@gmail.com>
2023-06-24 16:27:01 -0500

    Add the dependencies to be able to connect the generated services to the Eureka service, add @EnableDiscoveryClient annotation in the main class of the 3 services, add the configurations in the application.yml of the services to connect everything correctly.

--- ServiceQualification/src/main/java/com/qualification/ProjectQualifications/ProjectQualificationsApplication.java
+++ ServiceQualification/src/main/java/com/qualification/ProjectQualifications/ProjectQualificationsApplication.java
@@ -2,8 +2,10 @@ package com.qualification.ProjectQualifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProjectQualificationsApplication {
 
 	public static void main(String[] args) {
