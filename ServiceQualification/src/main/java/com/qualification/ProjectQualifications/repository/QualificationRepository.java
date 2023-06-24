0e88b0548699dd2a8b0e77a72265ec89e5bdd6bc
Luis Florez <braian1k3@gmail.com>
2023-06-24 14:46:05 -0500

    Add news queryMethods in the "QualificationRepository" and properties to connect Swagger documentation.

--- ServiceQualification/src/main/java/com/qualification/ProjectQualifications/repository/QualificationRepository.java
+++ ServiceQualification/src/main/java/com/qualification/ProjectQualifications/repository/QualificationRepository.java
@@ -3,7 +3,6 @@ package com.qualification.ProjectQualifications.repository;
 import com.qualification.ProjectQualifications.entity.Qualification;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
