b13be513bf7c124adf030642962f7f1035273ef8
Luis Florez <braian1k3@gmail.com>
2023-07-01 20:25:12 -0500

    Generate Config Server service for link new application properties and add configuration for service y connect wit of repository.

---
+++ ConfigServer/src/test/java/com/config/server/ProjectConfigServer/ProjectConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config.server.ProjectConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
