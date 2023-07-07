50bc89e846e81dc705e83e26497c6aaf204b2bc6
Luis Florez <braian1k3@gmail.com>
2023-07-07 16:09:00 -0500

    Generate new service to Auth JWT of Spring Security.

---
+++ AuthJWT/src/main/java/com/auth/jwt/ProjectAuthJWT/ProjectAuthJwtApplication.java
@@ -0,0 +1,13 @@
+package com.auth.jwt.ProjectAuthJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAuthJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAuthJwtApplication.class, args);
+	}
+
+}
---
+++ AuthJWT/src/test/java/com/auth/jwt/ProjectAuthJWT/ProjectAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.jwt.ProjectAuthJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
