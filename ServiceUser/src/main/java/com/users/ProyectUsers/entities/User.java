9b20cdcba6131aa91da94baf95d52ca5053b8bb7
Luis Florez <braian1k3@gmail.com>
2023-07-06 19:21:43 -0500

    Add @Builder for User.

--- ServiceUser/src/main/java/com/users/ProyectUsers/entities/User.java
+++ ServiceUser/src/main/java/com/users/ProyectUsers/entities/User.java
@@ -2,12 +2,14 @@ package com.users.ProyectUsers.entities;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "user_entity")
 public class User {
